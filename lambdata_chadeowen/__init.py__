357bfe2b42c8cae109b8d90fbf3f44ba3e188fb2
Chad Owen <chad.owen1@yahoo.com>
2019-02-12 11:12:14 -0500

    Add setup.py and basic pd/np in the package

---
+++ lambdata_chadeowen/__init.py__
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+VERSION = 0
+ONES = pd.ones(100)
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+""" Package setup/installation and metadata for lambdata
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-chadeowen",
+    version="0.0.2",
+    author="chadeowen",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chadeowen/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
